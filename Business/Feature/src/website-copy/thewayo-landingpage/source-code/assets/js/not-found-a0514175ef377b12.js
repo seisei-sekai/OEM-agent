17d2d1871c101014117a7f60ac6a23829b6c0bc6
NewbieAudioKid <zhangzsc10@gmail.com>
2026-01-23 20:40:19 +0900

    chore: Remove Business, Template, scripts and README_CN.md from version control
    - Remove Business/ folder (internal PRDs and feature documentation)
    - Remove Template/ folder (terraform and script templates)
    - Remove scripts/ folder (helper scripts)
    - Remove README_CN.md (Chinese documentation)
    - These files are now in .gitignore and will remain locally but not tracked by git

--- Business/Feature/src/website-copy/thewayo-landingpage/source-code/assets/js/not-found-a0514175ef377b12.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9160],{23602:function(n,e,u){Promise.resolve().then(u.t.bind(u,65469,23))}},function(n){n.O(0,[5469,1293,1362,8404,1744],function(){return n(n.s=23602)}),_N_E=n.O()}]);
--- Template/terraform/vm/deploy-app.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Application deployment script
-# Available variables: ${backend_url}
-
-set -e
-
-echo "Deploying ClassArranger Application"
-echo "Backend URL: ${backend_url}"
-
